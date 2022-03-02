1d417117c55acc468be4658c39a4691a0c46703a
nuruzzaman <mnueuzzaman@gmail.com>
2022-03-02 18:25:26 +0600

    api added

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ const allPhone = () =>{
 };
 
 const showPhoneDetails = (datas) =>{
+    
+    
     for(const data of datas ){
-        console.log(data);
         const parent = document.getElementById('phone-container');
         const div = document.createElement('div');
     div.innerHTML =`
@@ -17,16 +18,13 @@ const showPhoneDetails = (datas) =>{
     <div class="container">
     <div class="row">
       <div class="col-sm p-5">
-        <div id='color' class="card border">
+        <div  class="card border">
             <div class="pro-pic">
                 <img class="w-50" src="${data.image}" alt="">
             </div>
             <span>Phone Name: ${data.phone_name}</span>
             <span>Phone Brand: ${data.brand}</span>
-            <span>Storage: ${data.storage}</span>
-            <span>Display Size: ${data.displaySize}</span>
-            <span>ChipSet: ${data.chipSet}</span>
-            <span>Memory: ${data.memory}</span>
+            <span>Id: ${data.slug}</span>
             <div class="allbutton">
                 <button onclick='details("${data.slug}")' class="btn btn-info">Details</button>
             </div>
@@ -39,11 +37,7 @@ const showPhoneDetails = (datas) =>{
             </div>
             <span>Phone Name: ${data.phone_name}</span>
             <span>Phone Brand: ${data.brand}</span>
-            
-            <span>Storage: ${data.storage}</span>
-            <span>Display Size: ${data.displaySize}</span>
-            <span>ChipSet: ${data.chipSet}</span>
-            <span>Memory: ${data.memory}</span>
+            <span>Id: ${data.slug}</span>
             <div class="allbutton">
                 <button onclick='details("${data.slug}")' class="btn btn-info">Details</button>
             </div>
@@ -56,11 +50,7 @@ const showPhoneDetails = (datas) =>{
             </div>
             <span>Phone Name: ${data.phone_name}</span>
             <span>Phone Brand:${data.brand}</span>
-            
-            <span>Storage: ${data.storage}</span>
-            <span>Display Size: ${data.displaySize}</span>
-            <span>ChipSet: ${data.chipSet}</span>
-            <span>Memory: ${data.memory}</span>
+            <span>Id: ${data.slug}</span>
             <div class="allbutton">
                 <button onclick='details("${data.slug}")' class="btn btn-info">Details</button>
             </div>
@@ -83,7 +73,7 @@ const details = (id) =>{
 const setDetails = (info) =>{
     console.log(info);
     document.getElementById('details-container').innerHTML = `
-    <div class='border text-info'>
+    <div class='border'>
     <h3>Id: ${info.slug}</h3>
     <h3>Name: ${info.name}</h3>
     <h3>Releasedate: ${info.releaseDate}</h3>
